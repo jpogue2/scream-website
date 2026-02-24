d65fb21bb06db9a9374a85718547713d2fec2765
Jeremy Pogue <jpogue@usc.edu>
2026-02-23 17:36:44 -0800

    Attempt fix deploy #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   output : "static",
@@ -9,5 +11,7 @@ export default defineConfig({
     plugins: [
       tailwindcss()
     ]
-  }
+  },
+
+  adapter: netlify()
 });
